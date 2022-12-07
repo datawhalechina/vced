8a25410e7439837128389865781d4b96c0d56242
nil <nil@nil.com>
2022-12-07 08:29:31 +0800

    update shell

--- startup.sh
+++ startup.sh
@@ -5,5 +5,5 @@ if [ `arch` = "arm32" -o `arch` = "arm64" -o `arch` = "aarch64" ]; then
   docker-compose -f docker-compose-arm.yml up -d
 else
   docker-compose build
-  docker-compose -f docker-compose.yml up -d
+  docker-compose up -d
 fi
