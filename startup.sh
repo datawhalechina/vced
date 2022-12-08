a452f9e5edc59eee53797f817a48cf42d4443973
kainstan <hua-zai@qq.com>
2022-12-08 11:26:37 +0800

    update select nativa or docker

--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ `arch` = "arm32" -o `arch` = "arm64" -o `arch` = "aarch64" ]; then
-  docker-compose -f docker-compose-arm.yml build
-  docker-compose -f docker-compose-arm.yml up -d
-else
-  docker-compose build
-  docker-compose up -d
+if [ "$1" == "" -o "$1" == "docker" ]; then
+  if [ `arch` = "arm32" -o `arch` = "arm64" -o `arch` = "aarch64" ]; then
+    docker-compose -f docker-compose-arm.yml build
+    docker-compose -f docker-compose-arm.yml up -d
+  else
+    docker-compose build
+    docker-compose up -d
+  fi
+elif [ "$1" == "native" ]; then
+  echo "使用原生指令安装环境"
 fi
