8c8804595bcfb94a2eab073ec1979cc7a30b2400
Subranium <supeng842499467@gmail.com>
2023-04-23 10:19:02 +0800

    Update 7.4 切割大型图像.py

--- code/jina_demo/image/7.4 切割大型图像.py
+++ code/jina_demo/image/7.4 切割大型图像.py
@@ -4,10 +4,10 @@ d = Document(uri='complicated-image.jpeg')
 d.load_uri_to_image_tensor()
 print(d.tensor.shape)  # (792, 1000, 3)
 
-d.convert_image_tensor_to_sliding_windows(window_shape=(64, 64))  # 使用 64*64 的滑窗切割原图像，切分出 12*15=180 个图像张量
+d.convert_image_tensor_to_sliding_windows(window_shape=(64, 64))  # 使用 64*64 的滑窗切割原图像，切分出 13*16=208 个图像张量
 print(d.tensor.shape)  # (208, 64, 64, 3)
 
-# 可以通过 as_chunks=True，使得上述 180 张图片张量添加到 Document 块中。
+# 可以通过 as_chunks=True，使得上述 208 张图片张量添加到 Document 块中。
 # PS：运行这行代码时，需要重新 load image tensor，否则会报错。
 d = Document(uri='complicated-image.jpeg')
 d.load_uri_to_image_tensor()
